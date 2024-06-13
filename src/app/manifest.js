1b814231c50fb1391026ee70797e2762797bcffe
itSuhsko14 <itsushkoandriy@gmail.com>
2024-06-13 18:09:22 +0300

    add icons to manifest

--- src/app/manifest.js
+++ src/app/manifest.js
@@ -1,18 +1,43 @@
 export default function manifest() {
-    return {
-      name: 'Monitoring',
-      short_name: 'Monitoring',
-      description: 'Monitoring',
-      start_url: '/',
-      display: 'standalone',
-      background_color: '#fff',
-      theme_color: '#fff',
-      icons: [
-        {
-          src: 'icon-24x24.png',
-          sizes: '24x24',
-          type: 'image/png',
-        },
-      ],
-    }
-  }
+  return {
+    name: 'Your App Name',
+    short_name: 'App',
+    description: 'A brief description of your app',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#ffffff',
+    theme_color: '#4285F4', // Наприклад, синій колір теми
+    icons: [
+      {
+        src: 'icon-24x24.png',
+        sizes: '24x24',
+        type: 'image/png',
+        purpose: 'any',
+      },
+      {
+        src: 'icon-48x48.png',
+        sizes: '48x48',
+        type: 'image/png',
+        purpose: 'any',
+      },
+      {
+        src: 'icon-72x72.png',
+        sizes: '72x72',
+        type: 'image/png',
+        purpose: 'any',
+      },
+      {
+        src: 'icon-96x96.png',
+        sizes: '96x96',
+        type: 'image/png',
+        purpose: 'any',
+      },
+      {
+        src: 'icon-144x144.png',
+        sizes: '144x144',
+        type: 'image/png',
+        purpose: 'any',
+      },
+    ],
+  };
+}
