52830d2df35e863035c0d27b9ff3a24023351d04
itSuhsko14 <itsushkoandriy@gmail.com>
2024-06-13 18:09:44 +0300

    clear page.js

--- src/app/page.js
+++ src/app/page.js
@@ -10,18 +10,15 @@ import AppLoader from './../components/Loader/AppLoader';
 import TurnOffAll from "@/components/TurnOffAll/TurnOffAll";
 import { sendTelegram } from "@/components/SocketConection";
 import Head from 'next/head';
-import Login from './../components/Login/Login'
 
 const DeviceUi = dynamic(() => import('@/components/DeviceUI/DeviceUI'));
 const Auth = dynamic(() => import('@/components/Login/Auth'));
 
-export default function Home() {
-
+const Home = () => {
   const devices = useSelector(state => state.devices)
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    console.log('useEffect setLoading', devices.length === 0)
     setLoading(devices.length === 0);
   }, [devices]);
 
@@ -33,7 +30,6 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="container mx-auto py-4" style={{ maxWidth: '576px', minWidth: '320px' }}>
-        {/* <Login /> */}
         <SocketConection />
         <div className="list-group">
 
@@ -46,20 +42,16 @@ export default function Home() {
               {/* <MyButton buttonText={'Відправити повідомлення в телеграм'} handleOnClick={sendTelegram}/> */}
               <DeviceUi
                 devices={devices}
-              // key={device.id}
-              // deviceName={device.name}
-              // ip={device.ip}
-              // discs={device.disk}
-              // deviceId={device.id}
-              // device={device}
+
               />
-              <Auth />
+
             </>
           )}
-
-
+          <Auth />
         </div>
       </main>
     </>
   );
 }
+
+export default React.memo(Home);
