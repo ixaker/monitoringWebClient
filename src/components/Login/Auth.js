6953d221a57f903a7e6bb79aee40368fc52b9a16
itSuhsko14 <itsushkoandriy@gmail.com>
2024-06-13 18:11:04 +0300

    refactor Auth

--- src/components/Login/Auth.js
+++ src/components/Login/Auth.js
@@ -1,15 +1,14 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Login from './Login';
 import LogOut from './LogOut';
 
-const AuthComponent = ({}) => {
-    
+const AuthComponent = ({ token }) => {
     const [show, setShow] = useState(false)
     useEffect(() => {
         const token = localStorage.getItem('token');
         setShow(!token);
-    },[]);
-    return !show ? <LogOut setShow={setShow} /> : <Login setShow={setShow} show={show}/>;
+    }, [token]);
+    return !show ? <LogOut setShow={setShow} /> : <Login setShow={setShow} show={show} />;
 };
 
 export default AuthComponent;
